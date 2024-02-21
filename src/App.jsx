10d7f25baad612e6a1e37e07b824942656a3fb11
Austin <austinematurure@gmail.com>
2024-02-21 06:59:51 -0800

    fixed week description issue

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function Home() {
     "GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA", "2SA", "1KI", "2KI", "1CH", "2CH", "EZR", "NEH", "EST", "JOB", "PSA", "PRO", "ECC", "SON", "ISA", "JER", "LAM", "EZE", "DAN", "HOS", "JOE", "AMO", "OBA", "JON", "MIC", "NAH", "HAB", "ZEP", "HAG", "ZEC", "MAL", "MAT", "MAR", "LUK", "JOH", "ACT", "ROM", "1CO", "2CO", "GAL", "EPH", "PHI", "COL", "1TH", "2TH", "1TI", "2TI", "TIT", "PHM", "HEB", "JAM", "1PE", "2PE", "1JO", "2JO", "3JO", "JUD", "REV"
   ];
   const [names, setNames] = useState(['Mama', 'B', 'Parthe','Muno']);
-  const [week, setWeek] = useState('Next')
+  const [week, setWeek] = useState('This')
   const [cweek, setcWeek] = useState(2)
 
   const handleAddSevenDays = () => {
